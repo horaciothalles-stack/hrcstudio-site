0f883c85a1748ad44e3fff5464b5cf25e24e748c
horaciothalles-stack <horaciothalles@gmail.com>
2026-04-30 21:12:47 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,3 +7,10 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig();
+import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+
+export default defineConfig({
+  vite: {
+    base: '/',
+  }
+});
